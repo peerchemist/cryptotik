5584ec8c6aa8e6567b3ddd286c1c7305fad070a3
Peerchemist <dx.1178@gmail.com>
2017-01-16 19:32:56 +0100

    fix init

--- cryptotik/__init__.py
+++ cryptotik/__init__.py
@@ -4,8 +4,5 @@ from cryptotik.bittrex import Bittrex
 from cryptotik.btce import Btce
 from cryptotik.therock import TheRock
 from cryptotik.livecoin import Livecoin
-<<<<<<< HEAD
 from cryptotik.okcoin import OKcoin
-=======
 from cryptotik.hitbtc import Hitbtc
->>>>>>> 7e948ea7ab42a9ad57d9ec1259539995ff34fb34
