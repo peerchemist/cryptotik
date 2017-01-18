40f34cb8a8f95b43f5e840f862847d76b35789ac
Peerchemist <dx.1178@gmail.com>
2017-01-18 10:24:13 +0100

    common: add content type headers

--- cryptotik/common.py
+++ cryptotik/common.py
@@ -4,12 +4,13 @@
 class APIError(Exception):
     "Raise exception when the remote API returned an error."
     pass
-    
+
 headers = {    # common HTTPS headers
     'Accept': 'application/json',
     'Accept-Charset': 'utf-8',
     'Accept-Encoding': 'identity',
     'Cache-Control': 'no-cache',
-    'Connection': 'keep-alive'
+    'Connection': 'keep-alive',
+    "Content-type" : "application/x-www-form-urlencoded"
     }
 
