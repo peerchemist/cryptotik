bedefd83fe035ee3f0c721eb5896c4969948d334
Peerchemist <dx.1178@gmail.com>
2017-01-26 23:08:02 +0100

    btce: minor cleanup

--- cryptotik/btce.py
+++ cryptotik/btce.py
@@ -46,16 +46,18 @@ class Btce:
 
     @classmethod
     def get_markets(cls):
+        '''get all pairs supported by the exchange'''
+
         q = cls.api("info")
         return list(q['pairs'].keys())
-    
+
     @classmethod
     def get_market_ticker(cls, pair):
         """return ticker for market"""
-        
+
         pair = cls.format_pair(pair)
         return cls.api("ticker" + "/" + pair)
-    
+
     @classmethod
     def get_markets_ticker(cls):
         """return ticker for all pairs"""
@@ -66,27 +68,27 @@ class Btce:
     @classmethod
     def get_market_orders(cls, pair, depth=None):
         """returns market order book on selected pair"""
-        
+
         pair = cls.format_pair(pair)
 
         if depth == None:
             return cls.api("depth" + "/" + pair)[pair]
-        
+
         if depth > 2000:
             raise ValueError("Btce API allows maximum depth of 2000 orders")
-        
+
         return cls.api("depth" + "/" + pair + "/?limit={0}".format(depth))[pair]
-    
+
     @classmethod
     def get_market_trade_history(cls, pair, limit=1000):
         """get market trade history"""
-        
+
         pair = cls.format_pair(pair)
 
         if limit > 2000:
             raise APIError("Btc-e API can return only 2000 last trades.")
 
-        if type(pair) != list:
+        if not isinstance(pair, list):
             return cls.api("trades" + "/" + pair + "/?limit={0}".format(limit))[pair]
 
         if pair == "all": ## returns market history for all pairs with default history size.
@@ -94,8 +96,8 @@ class Btce:
 
         else: ## simply concat pairs in the list
             return cls.api("trades" + "/" + "-".join(pair) + "/?limit={0}".format(limit))
-    
-    @classmethod    
+
+    @classmethod
     def get_market_depth(cls, pair):
         """get market order book depth"""
 
@@ -103,9 +105,9 @@ class Btce:
 
         pair = cls.format_pair(pair)
         order_book = cls.get_market_orders(pair, 2000)
-        return {"bids": sum([Decimal(i[0]) * Decimal(i[1]) for i in order_book["bids"]]), 
+        return {"bids": sum([Decimal(i[0]) * Decimal(i[1]) for i in order_book["bids"]]),
                 "asks": sum([Decimal(i[1]) for i in order_book["asks"]])}
-    
+
     @classmethod
     def get_market_spread(cls, pair):
         """get market spread"""
@@ -116,4 +118,3 @@ class Btce:
         order_book = cls.get_market_orders(pair, 1)
         return Decimal(order_book["asks"][0][0]) - Decimal(order_book["bids"][0][0])
 
-        
