6c221ccf6b6efe480753a05b2a6babbc989e849b
Peerchemist <dx.1178@gmail.com>
2017-06-14 11:16:14 +0200

    error is dict, not string

--- test/poloniex_test.py
+++ test/poloniex_test.py
@@ -246,7 +246,7 @@ def test_buy(apikey, secret):
 
     polo = Poloniex(apikey, secret, 130)
 
-    assert polo.buy("btc-ppc", 0.000001, 0.001) == "{'error': 'Total must be at least 0.0001.'}"
+    assert polo.buy("btc-ppc", 0.000001, 0.001) == {'error': 'Total must be at least 0.0001.'}
 
 
 @private
@@ -255,7 +255,7 @@ def test_sell(apikey, secret):
 
     polo = Poloniex(apikey, secret, 130)
 
-    assert polo.sell("btc-ppc", 0.000001, 0.001) == "{'error': 'Total must be at least 0.0001.'}"
+    assert polo.sell("btc-ppc", 0.000001, 0.001) == {'error': 'Total must be at least 0.0001.'}
 
 
 @private
@@ -264,7 +264,7 @@ def test_margin_buy(apikey, secret):
 
     polo = Poloniex(apikey, secret, 130)
 
-    assert polo.margin_buy("btc-eth", 0.000001, 0.001) == "{'error': 'Total must be at least 0.0001.'}"
+    assert polo.margin_buy("btc-eth", 0.000001, 0.001) == {'error': 'Total must be at least 0.0001.'}
 
 
 @private
@@ -273,5 +273,5 @@ def test_margin_sell(apikey, secret):
 
     polo = Poloniex(apikey, secret, 130)
 
-    assert polo.margin_sell("btc-ppc", 0.000001, 0.001) == "{'error': 'Total must be at least 0.0001.'}"
+    assert polo.margin_sell("btc-ppc", 0.000001, 0.001) == {'error': 'Total must be at least 0.0001.'}
 
