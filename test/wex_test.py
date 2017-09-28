78bea3695bd5f40e88ba690fe2c38c404ce1b690
Peerchemist <dx.1178@gmail.com>
2017-09-28 18:13:51 +0200

    wex tests: add tests for private methods.

--- test/wex_test.py
+++ test/wex_test.py
@@ -1,5 +1,11 @@
+import pytest
 from cryptotik import Wex
 from decimal import Decimal
+
+private = pytest.mark.skipif(
+    not pytest.config.getoption("--apikey"),
+    reason="needs --apikey option to run."
+)
 
 
 def test_format_pair():
@@ -59,3 +65,70 @@ def test_get_market_spread():
 
     assert isinstance(Wex.get_market_spread("ppc_usd"), Decimal)
 
+
+@private
+def test_get_balances(apikey, secret):
+
+    wex = Wex(apikey, secret)
+    balances = wex.get_balances()
+
+    assert isinstance(balances, dict)
+    assert 'btc' in balances.keys()
+
+
+@private
+def test_get_deposit_address(apikey, secret):
+
+    wex = Wex(apikey, secret)
+
+    try:
+        with pytest.raises(ValueError):
+            wex.get_deposit_address("ltc")
+    except:
+        assert isinstance(wex.get_deposit_address("ppc"), dict)
+    
+
+@private
+def test_cancel_order(apikey, secret):
+
+    wex = Wex(apikey, secret)
+
+    with pytest.raises(ValueError):
+        wex.get_open_orders("ppc_btc")
+
+
+@private
+def test_buy(apikey, secret):
+
+    wex = Wex(apikey, secret)
+
+    with pytest.raises(ValueError):
+        wex.buy("ppc_btc", 0.05, 1)
+
+
+@private
+def test_sell(apikey, secret):
+
+    wex = Wex(apikey, secret)
+
+    with pytest.raises(ValueError):
+        wex.sell("ltc_btc", 1, 0.25)
+
+
+@private
+def test_get_transaction_history(apikey, secret):
+
+    wex = Wex(apikey, secret)
+
+    assert isinstance(wex.test_get_transaction_history, dict)
+
+
+@pytest.mark.xfail
+@private
+def test_withdraw(apikey, secret):
+
+    print('This is made to fail with <<api key dont have withdraw permission>>, so make sure your testing API key does not allow that.')
+    wex = Wex(apikey, secret)
+
+    with pytest.raises(ValueError):
+        wex.withdraw("ppc", 1, 'PpcEaT3Rd0NTsendftMKDAKr331DXgHe3L')
