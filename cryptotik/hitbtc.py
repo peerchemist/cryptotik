32214358bb40a71f2dee72eabed1c4081d872731
i-petko <ivan.petkovic8@gmail.com>
2017-11-02 21:44:29 +0100

    Hitbtc: new API, added private methods and test

--- cryptotik/hitbtc.py
+++ cryptotik/hitbtc.py
@@ -1,26 +1,57 @@
 # -*- coding: utf-8 -*-
 
+''' Hitbct exchange '''
+
+import time
 import requests
+from decimal import Decimal
 from cryptotik.common import APIError, headers, ExchangeWrapper
 
-class Hitbtc(ExchangeWrapper):
 
-    url = 'http://api.hitbtc.com/api/1/public/'
+class Hitbtc(ExchangeWrapper):
+    ''' Hitbct exchange '''
+
+    url = 'https://api.hitbtc.com/api/2/'
     delimiter = ""
     headers = headers
 
+    api_session = requests.Session()
+
+    def __init__(self, apikey=None, secret=None, timeout=None):
+        '''initialize object from Hitbtc class'''
+
+        if apikey and secret:
+            self.apikey = apikey.encode("utf-8")
+            self.secret = secret.encode("utf-8")
+    try:
+        assert timeout is not None
+    except:
+        timeout = (8, 15)
+
+    def get_nonce(self):
+        '''return nonce integer'''
+
+        nonce = getattr(self, '_nonce', 0)
+        if nonce:
+            nonce += 1
+        # If the unix time is greater though, use that instead (helps low
+        # concurrency multi-threaded apps always call with the largest nonce).
+        self._nonce = max(int(time.time()), nonce)
+        return self._nonce
+
     @classmethod
     def format_pair(cls, pair):
-        """format the pair argument to format understood by remote API."""
+        '''Format the pair argument to format understood by remote API.'''
+
         pair = pair.replace("-", cls.delimiter).upper()
         return pair
 
     @classmethod
-    def api(cls, url):
+    def api(cls, url, params={}):
         '''call api'''
 
         try:
-            result = requests.get(url, headers=cls.headers, timeout=3)
+            result = requests.get(url, params=params, headers=cls.headers, timeout=3)
             assert result.status_code == 200
             return result.json()
         except requests.exceptions.RequestException as e:
@@ -30,44 +61,161 @@ class Hitbtc(ExchangeWrapper):
     def get_market_ticker(cls, pair):
         '''returns simple current market status report'''
 
-        return cls.api(cls.url + cls.format_pair(pair) + "/ticker")
+        return cls.api(cls.url + "public/"+ "ticker/" + cls.format_pair(pair))
 
     @classmethod
-    def get_market_trade_history(cls, pair):
+    def get_market_trade_history(cls, pair, limit=1000):
         '''get market trade history'''
 
-        return cls.api(cls.url + cls.format_pair(pair) \
-        + "/trades/recent?max_results=1000&format_item=object")
+        return cls.api(cls.url + "public/trades/" + cls.format_pair(pair), 
+                        params={'limit': limit})
 
     @classmethod
-    def get_market_order_book(cls, pair):
+    def get_market_orders(cls, pair):
         '''return order book for the market'''
 
-        return cls.api(cls.url + cls.format_pair(pair) \
-        + "/orderbook?format_price=number&format_amount=number")
+        return cls.api(cls.url + "public/orderbook/" + cls.format_pair(pair))
 
     @classmethod
     def get_market_spread(cls, pair):
         '''return first buy order and first sell order'''
 
+<<<<<<< HEAD
+=======
         from decimal import Decimal
 
-        order_book = cls.get_market_order_book(pair)
+>>>>>>> ae66876... Hitbtc: updated to new API, added private methods and test
+        order_book = cls.get_market_orders(cls.format_pair(pair))
 
-        ask = order_book["asks"][0][0]
-        bid = order_book["bids"][0][0]
+        ask = order_book['ask'][0]['price']
+        bid = order_book['bid'][0]['price']
 
         return Decimal(ask) - Decimal(bid)
+
+
+    @classmethod
+    def get_markets(cls):
+        '''Find supported markets on this exchange'''
+
+        r = cls.api(cls.url + "public/" + "symbol")
+
+        pairs = [i["id"].lower() for i in r]
+        return pairs
+
+    @classmethod
+    def get_market_volume(cls, pair):
+        ''' return volume of last 24h'''
+
+        return cls.get_market_ticker(cls.format_pair(pair))["volume"]
 
     @classmethod
     def get_market_depth(cls, pair):
         '''return sum of all bids and asks'''
 
+<<<<<<< HEAD
+=======
         from decimal import Decimal
 
-        order_book = cls.get_market_order_book(pair)
-        asks = sum([Decimal(i[1]) for i in order_book["asks"]])
-        bid = sum([Decimal(i[0]) * Decimal(i[1]) for i in order_book["bids"]])
+>>>>>>> ae66876... Hitbtc: updated to new API, added private methods and test
+        order_book = cls.get_market_orders(cls.format_pair(pair))
+        asks = sum([Decimal(i['size']) for i in order_book['ask']])
+        bid = sum([Decimal(i['size']) for i in order_book['bid']])
 
         return {"bids": bid, "asks": asks}
 
+    def private_api(self, url, params={}, http_method='GET'):
+        '''handles private api methods'''
+
+        if not self.apikey or not self.secret:
+            raise ValueError("A Key and Secret needed!")
+
+        if http_method == 'GET':
+            result = requests.get(url, auth=(self.apikey, self.secret))
+        elif http_method == 'POST':
+            if not bool(params):
+                raise AttributeError("Data parameters required for POST request")
+            result = requests.post(url, data=params , auth=(self.apikey, self.secret))
+        elif http_method == 'PUT':
+            result = requests.put(url, auth=(self.apikey, self.secret))
+        elif http_method == 'DELETE':
+            result = requests.delete(url, auth=(self.apikey, self.secret))
+        assert result.status_code == 200, {'error: ' + str(result.json()['error'])}
+        return result.json()
+
+    def get_balances(self):
+        """get all balances from your account"""
+
+        balances = self.private_api(self.url + "trading/balance")
+
+        return [i for i in balances if i["available"] != '0']
+
+    def get_withdraw_history(self, currency):
+        '''Retrieves withdrawal history.'''
+
+        transactions = self.private_api(self.url + "account/transactions", 
+                                        params={"currency": currency.upper()})
+        return [i for i in transactions if i["type"] == 'payout']
+    
+    def withdraw(self, coin, amount, address):
+        '''withdraw <coin> <amount> to <address>'''
+
+        return self.private_api(self.url + "account/crypto/withdraw",
+                                params={"currency": coin.upper(),
+                                        "amount": amount,
+                                        "address": address},
+                                http_method='POST')
+
+    def get_order(self, order_id):
+        ''' Retreive a single order by it's ID '''
+
+        return self.private_api(self.url + "history/order/" + order_id + "/trades")
+
+    def sell(self, pair, price, quantity):
+        '''creates sell order for <pair> at <price> for <quantity>'''
+
+        return self.private_api(self.url + "order",
+                                params={'symbol': self.format_pair(pair),
+                                        'side': 'sell', 'quantity': quantity,
+                                        'price': price},
+                                http_method='POST')
+
+    def cancel_all_orders(self):
+        '''cancel all active orders'''
+
+        return self.private_api(self.url + "order", http_method='DELETE')
+
+    def cancel_order(self, clientOrderId):
+        '''cancels order with id <clientOrderId>'''
+
+        return self.private_api(self.url + "order/" + clientOrderId,
+                                http_method='DELETE')
+
+    def get_deposit_history(self, currency):
+        '''Retreive deposit history.'''
+
+        transactions = self.private_api(self.url + "account/transactions", 
+                                            params={"currency": currency.upper()})
+        return [i for i in transactions if i["type"] == 'payin']
+
+    def get_open_orders(self, symbol=None):
+        '''get open orders for <market>
+           or all open orders if called without an argument.'''
+
+        if symbol:
+            return self.private_api(self.url + "order", 
+                                    params={'symbol': symbol.upper()})
+        return self.private_api(self.url + "order")
+
+    def get_deposit_address(self, currency):
+        ''' get deposit address for <currency> '''
+
+        return self.private_api(self.url + "account/crypto/address/" + currency.upper())
+
+    def buy(self, pair, price, quantity):
+        '''creates buy order for <pair> at <price> for <quantity>'''
+
+        return self.private_api(self.url + "order",
+                                params={'symbol': self.format_pair(pair),
+                                    'side': 'buy', 'quantity': quantity,
+                                    'price': price},
+                                http_method='POST')
---
+++ test/hitbtc_test.py
@@ -0,0 +1,117 @@
+import pytest
+from cryptotik import Hitbtc
+from decimal import Decimal
+
+private = pytest.mark.skipif(
+    not pytest.config.getoption("--apikey"),
+    reason="needs --apikey option to run."
+)
+
+
+def test_format_pair():
+    '''test string formating to match API expectations'''
+
+    assert Hitbtc.format_pair("ppc-usd") == "PPCUSD"
+
+def test_get_markets():
+    '''test get_markets'''
+
+    assert isinstance(Hitbtc.get_markets(), list)
+    assert "ppcusd" in Hitbtc.get_markets()
+
+def test_get_market_ticker():
+    '''test get_market_ticker'''
+
+    ticker = Hitbtc.get_market_ticker("PPC-USD")
+
+    assert isinstance(ticker, dict)
+    assert sorted(ticker.keys()) == ['ask', 'bid', 'high', 'last', 'low', 'open', 'symbol', 'timestamp', 'volume', 'volumeQuote']
+
+def test_get_market_orders():
+    '''test get_market_orderbook'''
+
+    market_orders = Hitbtc.get_market_orders("ppc-usd")
+
+    assert isinstance(market_orders, dict)
+    assert isinstance(market_orders["ask"], list)
+    assert isinstance(market_orders["bid"], list)
+
+def test_get_market_trade_history():
+    '''test get_market_trade_history'''
+
+    trade_history = Hitbtc.get_market_trade_history("ppc-usd", 10)
+
+    assert isinstance(trade_history, list)
+    assert len(trade_history) == 10
+    assert sorted(trade_history[0].keys()) == sorted(['id', 'price', 'quantity', 'side', 'timestamp'])
+
+def test_get_market_depth():
+    '''test get_market_depth'''
+
+    market_depth = Hitbtc.get_market_depth("ppc-usd")
+
+    assert isinstance(market_depth, dict)
+    assert isinstance(market_depth["asks"], Decimal)
+
+def test_get_market_spread():
+    '''test get_market spread'''
+
+    assert isinstance(Hitbtc.get_market_spread("ppc-usd"), Decimal)
+
+@private
+def test_get_balances(apikey, secret):
+
+    hit = Hitbtc(apikey, secret)
+    balances = hit.get_balances()
+
+    assert isinstance(balances, list)
+
+@private
+def test_get_deposit_address(apikey, secret):
+
+    hit = Hitbtc(apikey, secret)
+
+    assert isinstance(hit.get_deposit_address("ppc"), dict)
+
+@private
+def test_get_withdraw_history(apikey, secret):
+
+    hit = Hitbtc(apikey, secret)
+
+    assert isinstance(hit.get_withdraw_history("ppc"), list)
+
+@pytest.mark.xfail
+@private
+def test_withdraw(apikey, secret):
+
+    print('This is made to fail with <Insufficient funds>' + 
+            ', so make sure your testing API key does not allow that.')
+    hit = Hitbtc(apikey, secret)
+
+    with pytest.raises(AssertionError):
+        hit.withdraw("ppc", 1, 'PpcEaT3Rd0NTsendftMKDAKr331DXgHe3L')
+
+@private
+def test_buy(apikey, secret):
+
+    hit = Hitbtc(apikey, secret)
+
+    with pytest.raises(AssertionError):
+        hit.buy("ppc_btc", 0.05, 1)
+
+
+@private
+def test_sell(apikey, secret):
+
+    hit = Hitbtc(apikey, secret)
+
+    with pytest.raises(AssertionError):
+        hit.sell("ltc_btc", 1, 0.25)
+
+@private
+def test_cancel_order(apikey, secret):
+
+    hit = Hitbtc(apikey, secret)
+
+    with pytest.raises(AssertionError):
+        hit.cancel_order('invalid')
