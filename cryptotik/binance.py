0bc6788d37a29ae3a6b25128bfd83f932a0a881e
Peerchemist <dx.1178@gmail.com>
2018-01-30 13:25:24 +0100

    rework Binance class, add proxy support.

--- cryptotik/binance.py
+++ cryptotik/binance.py
@@ -19,99 +19,100 @@ class Binance(ExchangeWrapper):
     headers = headers
     taker_fee, maker_fee = 0.001, 0.001
 
-    def __init__(self, apikey=None, secret=None, timeout=None):
-        '''initialize object from Hitbtc class'''
+    def __init__(self, apikey=None, secret=None, timeout=None, proxy=None):
 
         if apikey and secret:
             self.apikey = apikey
             self.secret = bytes(secret.encode("utf-8"))
-    try:
-        assert timeout is not None
-    except:
-        timeout = (8, 15)
 
-    @classmethod
-    def api(cls, url, params):
+        if proxy:
+            assert proxy.startswith('https'), {'Error': 'Only https proxies supported.'}
+        self.proxy = {'https': proxy}
+
+        if not timeout:
+            self.timeout = (8, 15)
+        else:
+            self.timeout = timeout
+
+        self.api_session = requests.Session()
+
+    def api(self, url, params):
         '''call api'''
 
         try:
-            result = requests.get(url, params=params, headers=cls.headers, timeout=3)
+            result = self.api_session.get(url, params=params, headers=self.headers,
+                                          timeout=self.timeout, proxies=self.proxy)
             assert result.status_code == 200, {'error: ' + str(result.json())}
             return result.json()
         except requests.exceptions.RequestException as e:
             raise APIError(e)
 
     @classmethod
-    def format_pair(cls, pair):
+    def format_pair(self, pair):
         '''Format the pair argument to format understood by remote API.'''
 
-        pair = pair.replace("-", cls.delimiter).upper()
+        pair = pair.replace("-", self.delimiter).upper()
         return pair
 
-    @classmethod
-    def get_market_ticker(cls, pair):
+    def get_market_ticker(self, pair):
         '''returns simple current market status report'''
-        params=(('symbol', cls.format_pair(pair)),)
-        return cls.api(cls.url + "api/v1/ticker/24hr", params)
 
-    @classmethod
-    def get_market_trade_history(cls, pair, limit=100):
+        params = (('symbol', self.format_pair(pair)),)
+
+        return self.api(self.url + "api/v1/ticker/24hr", params)
+
+    def get_market_trade_history(self, pair, limit=100):
         '''get market trade history'''
 
-        return cls.api(cls.url + "api/v1/aggTrades", 
-                        params=(('symbol', cls.format_pair(pair)), ('limit', limit),))
+        return self.api(self.url + "api/v1/aggTrades", 
+                        params=(('symbol', self.format_pair(pair)), ('limit', limit),))
 
-    @classmethod
-    def get_summaries(cls):
+    def get_summaries(self):
         '''get summary of all active markets'''
 
-        return cls.api(cls.url + 'api/v1/ticker/24hr',
-                       params=())
+        return self.api(self.url + 'api/v1/ticker/24hr',
+                        params=())
 
-    @classmethod
-    def get_market_orders(cls, pair, limit=100):
+    def get_market_orders(self, pair, limit=100):
         '''return sum of all bids and asks'''
-        params=(('symbol', cls.format_pair(pair)), ('limit', limit),)
 
-        return cls.api(cls.url + 'api/v1/depth', params)
+        params = (('symbol', self.format_pair(pair)), ('limit', limit),)
 
-    @classmethod
-    def get_market_spread(cls, pair):
+        return self.api(self.url + 'api/v1/depth', params)
+
+    def get_market_spread(self, pair):
         '''return first buy order and first sell order'''
 
-        order_book = cls.get_market_orders(cls.format_pair(pair))
+        order_book = self.get_market_orders(self.format_pair(pair))
 
         ask = order_book['asks'][0][0]
         bid = order_book['bids'][0][0]
-        
+
         return Decimal(ask) - Decimal(bid)
 
-    @classmethod
-    def get_market_depth(cls, pair):
+    def get_market_depth(self, pair):
         '''return sum of all bids and asks'''
 
-        order_book = cls.get_market_orders(cls.format_pair(pair))
+        order_book = self.get_market_orders(self.format_pair(pair))
         asks = sum([Decimal(i[1]) for i in order_book['asks']])
         bid = sum([Decimal(i[1]) for i in order_book['bids']])
 
         return {"bids": bid, "asks": asks}
 
-    @classmethod
-    def get_markets(cls, filter=None):
+    def get_markets(self, filter=None):
         '''Find supported markets on this exchange,
             use <filter> if needed'''
 
-        r = cls.api(cls.url + "api/v1/ticker/allPrices", params=())
+        r = self.api(self.url + "api/v1/ticker/allPrices", params=())
         pairs = [i["symbol"].lower() for i in r]
         if filter:
             pairs = [p for p in pairs if filter in p]
         return pairs
 
-    @classmethod
-    def get_market_volume(cls, pair):
+    def get_market_volume(self, pair):
         ''' return volume of last 24h'''
 
-        return cls.get_market_ticker(cls.format_pair(pair))["volume"]
+        return self.get_market_ticker(self.format_pair(pair))["volume"]
 
     def private_api(self, url, params={}, http_method='GET'):
         '''handles private api methods'''
@@ -121,12 +122,17 @@ class Binance(ExchangeWrapper):
 
         query = requests.compat.urlencode(sorted(params.items()))
         query += "&timestamp={}".format(int(time.time() * 1000))
+
         signature = hmac.new(self.secret, query.encode("utf-8"),
-                            hashlib.sha256).hexdigest()
+                             hashlib.sha256).hexdigest()
+
         query += "&signature={}".format(signature)
-        result = requests.request(http_method,
-                                url + "?" + query,
-                                headers={"X-MBX-APIKEY": self.apikey})
+
+        result = self.api_session.request(http_method,
+                                          url + "?" + query,
+                                          headers={"X-MBX-APIKEY": self.apikey},
+                                          proxies=self.proxy)
+
         assert result.status_code == 200, {'error: ' + str(result.json())}
         return result.json()
 
@@ -142,14 +148,14 @@ class Binance(ExchangeWrapper):
 
         return self.private_api(self.url + "api/v3/order",
                                 params={'symbol': self.format_pair(pair),
-                                    'side': 'BUY', 'type': 'limit',
-                                    'timeInForce': 'GTC', 'quantity': quantity,
-                                    'price': price},
+                                        'side': 'BUY', 'type': 'limit',
+                                        'timeInForce': 'GTC', 'quantity': quantity,
+                                        'price': price},
                                 http_method='POST')
 
     def get_deposit_address(self, currency):
         ''' get deposit address for <currency> '''
-        
+
         return self.private_api(self.url + "/wapi/v3/depositAddress.html",
                                 params={'asset': currency.upper()},
                                 http_method='GET') 
@@ -168,9 +174,9 @@ class Binance(ExchangeWrapper):
 
         return self.private_api(self.url + "api/v3/order",
                                 params={'symbol': self.format_pair(pair),
-                                    'side': 'SELL', 'type': 'limit',
-                                    'timeInForce': 'GTC', 'quantity': quantity,
-                                    'price': price},
+                                        'side': 'SELL', 'type': 'limit',
+                                        'timeInForce': 'GTC', 'quantity': quantity,
+                                        'price': price},
                                 http_method='POST')
 
     def withdraw(self, coin, amount, address, address_tag=None):
@@ -184,6 +190,7 @@ class Binance(ExchangeWrapper):
                                             'name': coin.upper(),
                                             'amount': amount},
                                     http_method='POST')
+
         return self.private_api(self.url + "/wapi/v3/withdraw.html",
                                 params={'asset': coin.upper(),
                                         'address': address, 
@@ -195,9 +202,9 @@ class Binance(ExchangeWrapper):
 
         if currency:
             transactions = self.private_api(self.url + "/wapi/v3/withdrawHistory.html", 
-                                        params={"asset": currency.upper()})
+                                            params={"asset": currency.upper()})
         else:
-            transactions = self.private_api(self.url + 
+            transactions = self.private_api(self.url +
                                 "/wapi/v3/withdrawHistory.html")
 
         return [i for i in transactions["withdrawList"]]
@@ -210,7 +217,7 @@ class Binance(ExchangeWrapper):
                                             params={"asset": currency.upper()})
         else:
             transactions = self.private_api(self.url + "/wapi/v3/depositHistory.html")
-        
+
         return [i for i in transactions["depositList"]]
 
     def get_order(self, symbol, order_id):
@@ -230,7 +237,7 @@ class Binance(ExchangeWrapper):
                                 http_method='DELETE')
 
     def cancel_all_orders(self):
-    
+
         for order in self.get_open_orders():
             self.cancel_order(order['orderId'], order['symbol'])
 
