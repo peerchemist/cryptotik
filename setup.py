8016581a1e8a7e85ad3acdb257686ada7354aba0
Peerchemist <dx.1178@gmail.com>
2018-02-19 11:51:42 +0100

    version bump.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='cryptotik',
-      version='0.3.2',
+      version='0.32',
       description='Standardized common API for several cryptocurrency exchanges.',
       url='https://github.com/peerchemist/cryptotik',
       author='Peerchemist',
