c2d7dc79063fddd495b807a5f70797f727d7e7d6
Peerchemist <dx.1178@gmail.com>
2018-02-19 10:39:42 +0100

    remove OKcoin from init as they are not implemented properly yet

--- cryptotik/__init__.py
+++ cryptotik/__init__.py
@@ -3,8 +3,6 @@ from cryptotik.poloniex import Poloniex
 from cryptotik.bittrex import Bittrex
 from cryptotik.wex import Wex
 from cryptotik.therock import TheRock
-from cryptotik.livecoin import Livecoin
-from cryptotik.okcoin import OKcoin
 from cryptotik.hitbtc import Hitbtc
 from cryptotik.bitstamp import Bitstamp
 from cryptotik.binance import Binance
