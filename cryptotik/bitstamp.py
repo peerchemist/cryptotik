943b68f1ee5848dc3164bba631cadd02a866d677
Peerchemist <dx.1178@gmail.com>
2018-02-28 00:48:56 +0100

    BistampNormalized: implement format_pair.

--- cryptotik/bitstamp.py
+++ cryptotik/bitstamp.py
@@ -6,6 +6,7 @@ import requests
 from decimal import Decimal
 import time
 from cryptotik.common import headers, ExchangeWrapper, APIError
+from cryptotik.exceptions import InvalidBaseCurrencyError, InvalidDelimiterError
 import hmac
 import hashlib
 
@@ -321,3 +322,22 @@ class BitstampNormalized(Bitstamp):
 
     def __init__(self, apikey=None, secret=None, timeout=None, proxy=None):
         super(BitstampNormalized, self).__init__(apikey, secret, customer_id, timeout, proxy)
+
+    @classmethod
+    def format_pair(self, market_pair):
+        """
+        Expected input is quote - base.
+        Normalize the pair inputs and
+        format the pair argument to a format understood by the remote API."""
+
+        market_pair = market_pair.lower()  # bittrex takes lowercase
+
+        if "-" not in market_pair:
+            raise InvalidDelimiterError('Agreed upon delimiter is "-".')
+
+        quote, base = market_pair.split('-')
+
+        if base not in self.base_currencies:
+            raise InvalidBaseCurrencyError('''Expected input is quote-base, you have provided with {pair}'''.format(pair=market_pair))
+
+        return quote + self.delimiter + base  # for bistamp quote comes first
