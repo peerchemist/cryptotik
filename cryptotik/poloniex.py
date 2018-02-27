6aff36549c13cbcaefe6b382424baebb1dfa35a5
Peerchemist <dx.1178@gmail.com>
2018-02-28 00:35:18 +0100

    PoloniexNormalized: implement format_pair.

--- cryptotik/poloniex.py
+++ cryptotik/poloniex.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from cryptotik.common import APIError, OutdatedBaseCurrenciesError, headers, ExchangeWrapper
+from cryptotik.exceptions import InvalidBaseCurrencyError, InvalidDelimiterError
 import datetime, time
 import requests
 import hmac, hashlib
@@ -581,3 +582,20 @@ class PoloniexNormalized(Poloniex):
 
     def __init__(self, apikey=None, secret=None, timeout=None, proxy=None):
         super(PoloniexNormalized, self).__init__(apikey, secret, timeout, proxy)
+
+    @classmethod
+    def format_pair(self, market_pair):
+        """
+        Expected input is quote - base.
+        Normalize the pair inputs and
+        format the pair argument to a format understood by the remote API."""
+
+        if "-" not in market_pair:
+            raise InvalidDelimiterError('Agreed upon delimiter is "-".')
+
+        quote, base = market_pair.split('-')
+
+        if base not in self.base_currencies:
+            raise InvalidBaseCurrencyError('''Expected input is quote-base, you have provided with {pair}'''.format(pair=market_pair))
+
+        return base.upper() + self.delimiter + quote.upper()  # for poloniex quote comes second
