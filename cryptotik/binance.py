27b3cc440f0b29014be0c3f7a8750e4227d62782
Peerchemist <dx.1178@gmail.com>
2018-03-02 02:35:14 +0100

    BinanceNormalized:: implement get_market_spread and get_market_depth

--- cryptotik/binance.py
+++ cryptotik/binance.py
@@ -381,21 +381,21 @@ class BinanceNormalized(Binance, NormalizedExchangeWrapper):
             'asks': [[i[0], i[1]] for i in upstream['asks']]
         }
 
-    def get_market_spread(self, pair):
+    def get_market_spread(self, market):
         '''return first buy order and first sell order'''
 
-        order_book = self.get_market_orders(self.format_pair(pair))
+        order_book = super().get_market_orders(market)
 
         ask = order_book['asks'][0][0]
         bid = order_book['bids'][0][0]
 
         return Decimal(ask) - Decimal(bid)
 
-    def get_market_depth(self, pair):
+    def get_market_depth(self, market):
         '''return sum of all bids and asks'''
 
-        order_book = self.get_market_orders(self.format_pair(pair))
-        asks = sum([Decimal(i[1]) for i in order_book['asks']])
-        bid = sum([Decimal(i[1]) for i in order_book['bids']])
+        order_book = self.get_market_orders(market, 1000)
 
-        return {"bids": bid, "asks": asks}
+        return {"bids": sum([Decimal(i[0]) * Decimal(i[1]) for i in order_book["bids"]]),
+                "asks": sum([Decimal(i[1]) for i in order_book["asks"]])
+                }
