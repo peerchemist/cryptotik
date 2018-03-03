0c70afdf14fe9a2d5b2f3f81e1a9f28f8a9b7b74
Peerchemist <dx.1178@gmail.com>
2018-03-03 03:24:21 +0100

    fix name of the dependency

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(name='cryptotik',
       author_email='peerchemist@protonmail.ch',
       license='BSD',
       packages=['cryptotik'],
-      install_requires=['requests', 'six', 'dateutil'],
+      install_requires=['requests', 'six', 'python-dateutil'],
       tests_require=['pytest'],
       zip_safe=False)
