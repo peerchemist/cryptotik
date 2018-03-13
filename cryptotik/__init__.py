e3a57b09549712247e21359b043bc81870b943d3
Peerchemist <dx.1178@gmail.com>
2018-03-13 01:23:36 +0100

    init:: import normalized classes

--- cryptotik/__init__.py
+++ cryptotik/__init__.py
@@ -1,12 +1,12 @@
 
-from cryptotik.poloniex import Poloniex
-from cryptotik.bittrex import Bittrex
-from cryptotik.wex import Wex
-from cryptotik.therock import TheRock
-from cryptotik.hitbtc import Hitbtc
-from cryptotik.bitstamp import Bitstamp
-from cryptotik.binance import Binance
+from cryptotik.poloniex import Poloniex, PoloniexNormalized
+from cryptotik.bittrex import Bittrex, BittrexNormalized
+from cryptotik.wex import Wex, WexNormalized
+from cryptotik.therock import TheRock, TheRockNormalized
+from cryptotik.hitbtc import Hitbtc, HitbtcNormalized
+from cryptotik.bitstamp import Bitstamp, BitstampNormalized
+from cryptotik.binance import Binance, BinanceNormalized
 from cryptotik.bitkonan import Bitkonan
-from cryptotik.kraken import Kraken
-from cryptotik.cryptopia import Cryptopia
+from cryptotik.kraken import Kraken, KrakenNormalized
+from cryptotik.cryptopia import Cryptopia, CryptopiaNormalized
 from cryptotik.coinmarketcap import CoinMarketCap
