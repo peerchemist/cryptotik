cc8b6e3eb11ea2e86216a49b66b641fe41a8fa7b
Peerchemist <dx.1178@gmail.com>
2018-07-22 14:17:53 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,26 @@
 from setuptools import setup
+from os import path
+
+classifiers = [
+  'Development Status :: 4 - Beta',
+  'Intended Audience :: Financial and Insurance Industry',
+  'Programming Language :: Python',
+  'Operating System :: OS Independent',
+  'Natural Language :: English',
+  'License :: OSI Approved :: BSD License'
+]
+
+# read the contents of your README file
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 
 setup(name='cryptotik',
       version='0.34',
       description='Standardized common API for several cryptocurrency exchanges.',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       url='https://github.com/peerchemist/cryptotik',
       author='Peerchemist',
       author_email='peerchemist@protonmail.ch',
